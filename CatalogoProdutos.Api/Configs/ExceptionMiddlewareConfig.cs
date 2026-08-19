351c2142f9c5464d8e94ce5731440053a5483325
jmarcosalfredo <joao_marcos126@hotmail.com>
2026-08-19 16:07:39 -0300

    feat: Implement Exception Handler Middleware at production environment

---
+++ CatalogoProdutos.Api/Configs/ExceptionMiddlewareConfig.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CatalogoProdutos.Api.Models;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace CatalogoProdutos.Api.Configs
+{
+    public static class ExceptionMiddlewareConfig
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Trace = contextFeature.Error.StackTrace
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
--- CatalogoProdutos.Api/Program.cs
+++ CatalogoProdutos.Api/Program.cs
@@ -17,6 +17,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwaggerConfig();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
